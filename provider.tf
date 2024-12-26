15aff9813d60ec1de40c3aba555e873b2b646d1b
Hamza Nasir <101545759+hamzanasir47@users.noreply.github.com>
2024-12-26 15:22:53 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.0.0"
+      version = "4.14.0"
     }
   }
 }
@@ -10,6 +10,6 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-  version = "3.0.0"
-  use_azure_cli = true
+  version = "4.14.0"
+  #use_azure_cli = true
 }
